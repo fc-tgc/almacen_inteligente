91702dbab54986b7bf8c6927b4481c8473ab36ad
Federico Colangelo <fede.tgc.colangelo@gmail.com>
2026-09-22 22:15:26 -0300

    hilo camiones

--- src/bin/zona_recepcion.rs
+++ src/bin/zona_recepcion.rs
@@ -7,11 +7,13 @@ const CAPACIDAD_CINTA: usize = 10;
 
 struct Paquete {
     id: usize,
+    // faltan un par de campos en caso que tenga que ser consistente con la parte 1 del TP.
 }
 
 struct EstadoCinta { 
     buffer: VecDeque<Paquete>,
     paquetes_restantes: usize,
+    paquetes_procesados: usize,
  }
 struct ZonaAlmacenamiento { 
     camiones: usize,
@@ -19,4 +21,22 @@ struct ZonaAlmacenamiento {
     hay_paquetes: Condvar,
     cinta_llena: Condvar,
     estado: Mutex<EstadoCinta>,
+ }
+
+ fn rol_camion(id: usize, zona: Arc<ZonaAlmacenamiento>) {
+    loop {
+        let mut guard = zona.estado.lock().unwrap();
+        if guard.paquetes_restantes == 0 {
+            break;
+        }
+        guard = zona.cinta_llena.wait_while(guard, |e| e.buffer.len() == CAPACIDAD_CINTA).unwrap();
+        guard.paquetes_restantes -= 1;
+        guard.paquetes_procesados += 1;
+        let paquete = Paquete{id: guard.paquetes_procesados};
+        let id_paquete = paquete.id;
+        guard.buffer.push_back(paquete);
+        zona.hay_paquetes.notify_one();
+        drop(guard);
+        println!("Camión {}: dejó paquete {}", id, id_paquete);
+    }
  }
