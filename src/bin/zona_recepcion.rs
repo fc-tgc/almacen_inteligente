252b6609a3144648978f2fe1819b07f4bf5b5e31
Federico Colangelo <fede.tgc.colangelo@gmail.com>
2026-09-22 22:26:39 -0300

    hilo robotos

--- src/bin/zona_recepcion.rs
+++ src/bin/zona_recepcion.rs
@@ -40,3 +40,18 @@ struct ZonaAlmacenamiento {
         println!("Camión {}: dejó paquete {}", id, id_paquete);
     }
  }
+
+ fn rol_robot(id: usize, zona: Arc<ZonaAlmacenamiento>) {
+    loop {
+        let mut guard = zona.estado.lock().unwrap();
+        guard = zona.hay_paquetes.wait_while(guard, |e| e.buffer.len() == 0 && e.paquetes_restantes != 0).unwrap();
+        if guard.buffer.len() == 0 && guard.paquetes_restantes == 0 {
+            break;
+        }
+        let paquete = guard.buffer.pop_front().unwrap();
+        zona.cinta_llena.notify_one();
+        drop(guard);
+        println!("Robot {}: tomó paquete {}", id, paquete.id);
+ 
+    }
+}
